fd8e908274f8cc275ef94c1b2e91d1c9cbedb5ca
Luis Pereira <pereira.luishm@gmail.com>
2022-09-23 00:01:46 -0300

    refactor socket server implementation

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,23 +2,7 @@ import Message from "../models/Message";
 import { Server, Socket } from "socket.io";
 import app from "../app";
 
-const io = new Server(app, {
-  cors: {
-    origin: ["http://localhost:3000"],
-  },
-});
-
-class UserConnection {
-  _id: string;
-  socket: Socket;
-
-  constructor(_id: string, socket: Socket) {
-    this._id = _id;
-    this.socket = socket;
-  }
-}
-
-interface Message {
+interface IMessage {
   _id?: string;
   text: string;
   receiver: string;
@@ -26,59 +10,112 @@ interface Message {
   createdAt: number;
 }
 
-class UsersPool {
-  pool: UserConnection[];
+interface SocketChat {
+  send: (message: IMessage) => void;
+}
 
-  constructor() {
-    this.pool = [];
+class UserChat implements SocketChat {
+  _id: string;
+  socket: Socket;
+
+  constructor(_id: string, socket: Socket) {
+    this._id = _id;
+    this.socket = socket;
   }
 
-  add(id: string, socket: Socket) {
-    if (this.pool.filter((u) => u._id === id).length) return;
-    this.pool = [...this.pool, new UserConnection(id, socket)];
-    this.pool
-      .map((u) => u.socket)
-      .forEach((s) =>
-        s.emit(
-          "users-list",
-          this.pool.map((u) => u._id)
-        )
-      );
+  send(message: IMessage) {
+    this.socket.emit("receive-message", message);
   }
 
-  remove(socket: Socket) {
-    this.pool = this.pool.filter((u) => u.socket.id !== socket.id);
-    this.pool
-      .map((u) => u.socket)
-      .forEach((s) =>
-        s.emit(
-          "users-list",
-          this.pool.map((u) => u._id)
-        )
-      );
-  }
-
-  send(message: Message) {
-    const found = this.pool.find((u) => u._id === message.receiver);
-    delete message._id;
-    Message.create(message)
-    if (!found) return;
-    found.socket.emit("receive-message", message);
+  sendConnectedUsers(usersId: string[]) {
+    this.socket.emit("users-list", usersId);
   }
 }
 
-const users = new UsersPool();
+class GroupChat implements SocketChat {
+  _id: string;
+  users: UserChat[];
 
-io.on("connection", (socket) => {
-  socket.on("user-connected", (id: string) => {
-    users.add(id, socket);
-  });
+  constructor(_id: string) {
+    this._id = _id;
+    this.users = [];
+  }
 
-  socket.on("send-message", (message) => {
-    users.send(message);
-  });
+  add(user: UserChat) {
+    this.users = [...this.users.filter((u) => u._id !== user._id), user];
+  }
 
-  socket.on("disconnect", () => {
-    users.remove(socket);
-  });
-});
+  send(message: IMessage) {
+    this.users
+      .filter((u) => u._id !== message.sender)
+      .forEach((u) => u.send(message));
+  }
+}
+
+class ServerSocket {
+  users: UserChat[];
+  groups: GroupChat[];
+
+  constructor() {
+    this.users = [];
+    this.groups = [];
+
+    const io = new Server(app, {
+      cors: {
+        origin: ["http://localhost:3000"],
+      },
+    });
+
+    const self = this;
+
+    io.on("connection", (socket) => {
+      socket.on("user-connected", (id: string, groupsId: string[]) => {
+        self.add(new UserChat(id, socket), groupsId);
+        console.log(
+          "<<< Groups >>>",
+          this.groups.map((g) => g.users)
+        );
+      });
+
+      socket.on("send-message", (message: IMessage) => {
+        Message.create({ ...message, _id: undefined });
+
+        const user = self.users.find((u) => u._id === message.receiver);
+        if (user) return user.send(message);
+
+        const group = self.groups.find((g) => g._id === message.receiver);
+        if (group) return group.send(message);
+      });
+
+      socket.on("disconnect", () => {
+        self.remove(socket.id);
+      });
+    });
+  }
+
+  add(user: UserChat, groupsId: string[]) {
+    const unique = groupsId
+      .filter((gid) => !this.groups.map((g) => g._id).includes(gid))
+      .map((gid) => new GroupChat(gid));
+    this.groups = [...this.groups, ...unique];
+
+    this.users = [...this.users.filter((u) => u._id !== user._id), user];
+
+    this.groups.forEach((group) => {
+      if (groupsId.includes(group._id)) group.add(user);
+    });
+    this.broadcastConnectedUsersList();
+  }
+
+  remove(socketId: string) {
+    this.users = this.users.filter((u) => u.socket.id !== socketId);
+    this.broadcastConnectedUsersList();
+  }
+
+  broadcastConnectedUsersList() {
+    const usersId = this.users.map((u) => u._id);
+    this.users.forEach((u) => u.sendConnectedUsers(usersId));
+  }
+}
+
+new ServerSocket();
